f43530e71ae863487f739b2aaa031ca98e6df159
harada-webdev <hrdk74531@outlook.jp>
2024-09-20 20:22:59 +0900

    Add attributes to User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -11,5 +12,12 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource)
     new_user_session_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name address postal_code bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name address postal_code bio])
   end
 end
