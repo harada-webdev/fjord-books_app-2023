514ecfa86b576140a48e151e14e960f378d52925
harada-webdev <hrdk74531@outlook.jp>
2024-09-27 04:45:43 +0900

    Add function to delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy]
+
   def new; end
 
   def create
@@ -12,9 +14,18 @@ class CommentsController < ApplicationController
     redirect_to @commentable, notice: 'Comment was successfully created'
   end
 
+  def destroy
+    @comment.destroy
+    redirect_to @commentable, notice: 'Comment was successfully destroyed.'
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
