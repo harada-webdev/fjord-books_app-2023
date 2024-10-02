031274e6369f3af2c25bf7386f5b37128bdff17a
harada-webdev <hrdk74531@outlook.jp>
2024-10-03 04:46:30 +0900

    Associate Report with ReportMention

--- app/models/report_mention.rb
+++ app/models/report_mention.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ReportMention < ApplicationRecord
+  belongs_to :mentioning, class_name: 'Report'
+  belongs_to :mentioned, class_name: 'Report'
+
+  validates :mentioning_id, uniqueness: { scope: :mentioned_id }
 end
