53e93272eb385ebdb6e7ef63874df8234ea6a70c
harada-webdev <hrdk74531@outlook.jp>
2024-10-11 06:45:13 +0900

    Add Report CRUD system test

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -4,44 +4,45 @@ require 'application_system_test_case'
 
 class ReportsTest < ApplicationSystemTestCase
   setup do
-    @report = reports(:one)
-  end
+    @report = reports(:alice)
 
-  test 'visiting the index' do
-    visit reports_url
-    assert_selector 'h1', text: 'Reports'
+    visit root_path
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
+    assert_text 'ログインしました。'
   end
 
   test 'should create report' do
     visit reports_url
-    click_on 'New report'
+    click_on '日報の新規作成'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Create Report'
+    fill_in 'タイトル', with: 'HTMLとCSS'
+    fill_in '内容', with: 'HTMLとCSSの本を読みました'
+    click_button '登録する'
 
-    assert_text 'Report was successfully created'
-    click_on 'Back'
+    assert_text '日報が作成されました。'
+    assert_text 'HTMLとCSS'
+    assert_text 'HTMLとCSSの本を読みました'
   end
 
   test 'should update Report' do
     visit report_url(@report)
-    click_on 'Edit this report', match: :first
+    click_on 'この日報を編集'
 
-    fill_in 'Content', with: @report.content
-    fill_in 'Title', with: @report.title
-    fill_in 'User', with: @report.user_id
-    click_on 'Update Report'
+    fill_in 'タイトル', with: 'Rails'
+    fill_in '内容', with: 'Railsの本を読みました'
+    click_button '更新する'
 
-    assert_text 'Report was successfully updated'
-    click_on 'Back'
+    assert_text '日報が更新されました。'
+    assert_text 'Rails'
+    assert_text 'Railsの本を読みました'
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
-    click_on 'Destroy this report', match: :first
+    click_button 'この日報を削除'
 
-    assert_text 'Report was successfully destroyed'
+    assert_text '日報が削除されました。'
   end
 end
