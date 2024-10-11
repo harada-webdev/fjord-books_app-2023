31a9b0e6bacc1c7a6e9d1a0db28020caf00a63f8
harada-webdev <hrdk74531@outlook.jp>
2024-10-12 00:14:42 +0900

    Define mentioned_reports variable in Report controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,6 +9,7 @@ class ReportsController < ApplicationController
 
   def show
     @report = Report.find(params[:id])
+    @mentioned_reports = @report.mentioned_reports.order(:id).includes(mentioned_reports: :user)
   end
 
   # GET /reports/new
